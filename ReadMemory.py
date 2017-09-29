e96581a80c80bf9b82f799daf9495f45939b5870
Nicotrial <nicotrial@gmail.com>
2017-09-29 13:42:57 +0100

    Cleaned code

--- ReadMemory.py
+++ ReadMemory.py
@@ -9,20 +9,11 @@ pid = 1864  # explorer pid
 k32 = c.windll.kernel32
 
 OpenProcess = k32.OpenProcess
-OpenProcess.argtypes = [w.DWORD,w.BOOL,w.DWORD]
-OpenProcess.restype = w.HANDLE
-
 ReadProcessMemory = k32.ReadProcessMemory
-ReadProcessMemory.argtypes = [w.HANDLE,w.LPCVOID,w.LPVOID,c.c_size_t,c.POINTER(c.c_size_t)]
-ReadProcessMemory.restype = w.BOOL
-
+WriteProcessMemory = k32.WriteProcessMemory
 GetLastError = k32.GetLastError
-GetLastError.argtypes = None
-GetLastError.restype = w.DWORD
-
 CloseHandle = k32.CloseHandle
-CloseHandle.argtypes = [w.HANDLE]
-CloseHandle.restype = w.BOOL
+
 
 processHandle = OpenProcess(0x10, False, pid)
 print (processHandle)
@@ -48,5 +39,6 @@ while True:
 
 	print('result: {}, err code: {}, bytesRead: {}'.format(result,e,bytesRead.value))
 	print('data: {:016X}h'.format(data.value))
+	
 	time.sleep(0.01)
 CloseHandle(processHandle)
